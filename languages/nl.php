8b06caa364d87e12b5f4166fe5100d782598cdfe
Jerome Bakker <jeabakker@coldtrick.com>
2019-02-21 12:15:05 +0100

    chore(i18n): updated Dutch translation

--- languages/nl.php
+++ languages/nl.php
@@ -1,6 +1,18 @@
 <?php
+/**
+ * This file was created by Translation Editor v7.0-beta
+ * On 2019-02-21 12:14
+ */
 
-return [
+return array (
+  'mentions:upgrade:2019022100:title' => 'Migreer de mentions notificatie instellingen',
+  'mentions:upgrade:2019022100:description' => 'De notificatie instellingen worden nu opgeslagen per notificatiemethode, 
+migreer de oude instellingen naar de nieuwe.',
+  'mentions:notification_types:object:discussion' => 'een discussie',
+  'mentions:notification_types:object:comment' => 'een reactie',
+  'mentions:named_links' => 'Vervang @gebruikersnaam met de weergave naam van de gebruiker',
+  'mentions:restrict_group_search' => 'In groepen, beperk de autocomplete suggesties tot groepsleden',
+  'mentions:friends_only_search' => 'Beperk autocomplete suggesties tot vrienden',
   'mentions:notification:subject' => '%s heeft je genoemd in %s',
   'mentions:notification:body' => '%s heeft je genoemd in %s.
 
@@ -8,11 +20,7 @@ Om het volledige bericht te zien, klik op onderstaande link:
 %s',
   'mentions:notification_types:object:blog' => 'een blog post',
   'mentions:notification_types:object:bookmarks' => 'een favoriet',
-  'mentions:notification_types:object:groupforumtopic' => 'een discussie',
-  'mentions:notification_types:annotation:group_topic_post' => 'een discussiereactie',
   'mentions:notification_types:object:thewire' => 'een wire bericht',
-  'mentions:notification_types:annotation:generic_comment' => 'een reactie',
   'mentions:settings:send_notification' => 'Verstuur een notificatie als iemand je @vermeld in een bericht?',
   'mentions:fancy_links' => 'Vervang @vermeldingen met een kleine afbeelding van de gebruiker bovenop de gebruiker z\'n naam',
-  'mentions:settings:failed' => 'Er is een fout opgetreden tijdens het opslaan van de vermeldingen instellingen.',
-];
+);
