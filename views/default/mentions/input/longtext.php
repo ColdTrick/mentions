6ed1bf3d32c236e3bf23a4f57d13b2a4c2f4bed8
Jerome Bakker <jeabakker@coldtrick.com>
2019-02-21 12:20:05 +0100

    chore(views): support not loading WYSIWYG editor on longtext

--- views/default/mentions/input/longtext.php
+++ views/default/mentions/input/longtext.php
@@ -7,21 +7,23 @@ if (!elgg_extract('allow_mentions', $vars, true)) {
 	return;
 }
 
-$plugins = [
-	'ckeditor',
-	'tinymce',
-	'extended_tinymce',
-];
-
 $editor = 'plaintext';
-foreach ($plugins as $plugin) {
-	if (elgg_is_active_plugin($plugin)) {
-		$editor = $plugin;
+if (elgg_extract('editor', $vars, true)) {
+	$plugins = [
+		'ckeditor',
+		'tinymce',
+		'extended_tinymce',
+	];
+	
+	foreach ($plugins as $plugin) {
+		if (elgg_is_active_plugin($plugin)) {
+			$editor = $plugin;
+		}
 	}
-}
-
-if ($editor == 'extended_tinymce') {
-	$editor = 'tinymce';
+	
+	if ($editor == 'extended_tinymce') {
+		$editor = 'tinymce';
+	}
 }
 
 elgg_require_js("mentions/editors/$editor");
