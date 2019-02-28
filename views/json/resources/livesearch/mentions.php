b929cf6ae8b3272ae8d62d6910a0968e7aacdb93
Jerome Bakker <jeabakker@coldtrick.com>
2019-02-28 16:17:52 +0100

    chore: only find non banned users

--- views/json/resources/livesearch/mentions.php
+++ views/json/resources/livesearch/mentions.php
@@ -2,21 +2,24 @@
 
 elgg_gatekeeper();
 
-$target_guid = (int) get_input('target_guid');
-$target = get_entity($target_guid);
-
 // needed for correct output, but not used when handling results
-set_input('name', 'dummy_input');
+$vars['name'] = 'dummy_input';
 
+// don't mention banned users
+$vars['include_banned'] = false;
+
+// check for group, and plugin settings
+$target_guid = (int) elgg_extract('target_guid', $vars);
+$target = get_entity($target_guid);
 if ($target instanceof ElggGroup && elgg_get_plugin_setting('restrict_group_search', 'mentions')) {
 	// search only for group members
-	set_input('group_guid', $target_guid);
+	$vars['group_guid'] = $target_guid;
 	
-	echo elgg_view_resource('livesearch/group_members');
+	echo elgg_view_resource('livesearch/group_members', $vars);
 	return;
 } elseif (elgg_get_plugin_setting('friends_only_search', 'mentions')) {
-	echo elgg_view_resource('livesearch/friends');
+	echo elgg_view_resource('livesearch/friends', $vars);
 	return;
 }
 
-echo elgg_view_resource('livesearch/users');
+echo elgg_view_resource('livesearch/users', $vars);
