c01a847254c402fe091748029aa3b343449635d1
Jerome Bakker <jeabakker@coldtrick.com>
2019-07-04 09:50:33 +0200

    added: ckeditor 4.11

--- elgg-plugin.php
+++ elgg-plugin.php
@@ -3,6 +3,11 @@
 use Elgg\Mentions\Bootstrap;
 use Elgg\Mentions\Upgrades\MigrateNotificationSettings;
 
+$composer_dir = '';
+if (is_dir(__DIR__ . '/vendor')) {
+	$composer_dir = __DIR__ . '/';
+}
+
 return [
 	'bootstrap' => Bootstrap::class,
 	'routes' => [
@@ -19,5 +24,11 @@ return [
 	],
 	'upgrades' => [
 		MigrateNotificationSettings::class,
+	],
+	'views' => [
+		'default' => [
+			'ckeditor/' => $composer_dir . 'vendor/ckeditor/ckeditor/',
+			'jquery.ckeditor.js' => $composer_dir . 'vendor/ckeditor/ckeditor/adapters/jquery.js',
+		],
 	],
 ];
